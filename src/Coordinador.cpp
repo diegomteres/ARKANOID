b65349445ef21ac435b489bd52784e3ffcf9f7fe
diegomteres <38976324+diegomteres@users.noreply.github.com>
2018-05-31 17:37:08 +0200

    Add files via upload

--- src/Coordinador.cpp
+++ src/Coordinador.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "ETSIDI.h"
 #include "Coordinador.h"
 #include "glut.h"					//ORDEN DE INCLUSIÓN LIBS INFLUYE, PREGUNTAR
@@ -113,3 +114,120 @@ void Coordinador::mueve()
 
 
 
+=======
+#include "ETSIDI.h"
+#include "Coordinador.h"
+#include "glut.h"					//ORDEN DE INCLUSIÓN LIBS INFLUYE, PREGUNTAR
+//using namespace ETSIDI;
+
+Coordinador::Coordinador(void)
+{
+	estado=INICIO;
+}
+
+
+Coordinador::~Coordinador(void)
+{
+}
+
+void Coordinador::dibuja() 
+{   
+	if(estado==INICIO)  
+	{
+		gluLookAt(0, 7.5, 30,  // posicion del ojo  
+		   0.0, 7.5, 0.0,      // hacia que punto mira  (0,7.5,0)    
+		   0.0, 1.0, 0.0);     // definimos hacia arriba (eje Y)    
+		
+		ETSIDI::setTextColor(1,1,0);							//NO PINTA Y NO SE EQUISCU
+		ETSIDI::setFont("fuentes/Bitwise.ttf",16);  
+		ETSIDI::printxy("ARKANOID", -5,8);   
+
+		ETSIDI::setTextColor(1,1,1);   
+		ETSIDI::setFont("fuentes/Bitwise.ttf",12);  
+		ETSIDI::printxy("PULSE LA TECLA -E- PARA EMPEZAR", -5,7);  
+		ETSIDI::printxy("PULSE LA TECLA -S- PARA SALIR", -5,6);  
+	}
+	else if(estado==JUEGO)  
+		mundo.Dibuja();
+	else if(estado==GAMEOVER) 
+	{   
+		mundo.Dibuja();  
+		ETSIDI::setTextColor(1,0,0);  
+		ETSIDI::setFont("fuentes/Bitwise.ttf",16);   
+		ETSIDI::printxy("GAMEOVER: Has perdido",-5,10);
+		ETSIDI::printxy("Pulsa -C- para continuar",-5,5); 
+	}  
+	else if(estado==FIN) 
+	{ 
+		mundo.Dibuja();
+		ETSIDI::setFont("fuentes/Bitwise.ttf",16);  
+		ETSIDI::printxy("ENHORABUENA, ¡Has triunfado!",-5,10); 
+		ETSIDI::printxy("Pulsa -C- para continuar",-5,9); 
+	}
+}
+/*
+void Coordinador::tecla(unsigned char key) 
+{
+	if(estado==INICIO)  
+	{   
+		mundo.Inicializa();   
+		estado=JUEGO; 
+	}  
+	else if(estado==JUEGO) 
+	{   
+		mundo.tecla(key);
+	} 
+} 
+*/
+void Coordinador::Tecla(unsigned char key) 
+{  
+	if(estado==INICIO)  
+	{  
+		if(key=='s')   
+			exit(0); 
+		if(key=='e')   
+		{   
+			mundo.Inicializa();  
+			estado=JUEGO; 
+		} 
+	 
+	} 
+	else if(estado==JUEGO) 
+	{ 
+		mundo.tecla(key); 
+	} 
+	else if(estado==GAMEOVER) 
+	{  
+		if(key=='c') 
+			estado=INICIO;  
+	}  
+	else if(estado==FIN) 
+	{  
+		if(key=='c') 
+			estado=INICIO;  
+	}
+}
+
+
+void Coordinador::teclaEspecial(unsigned char key) 
+{  
+	if(estado==JUEGO) 
+		mundo.teclaEspecial(key);
+} 
+
+void Coordinador::teclaEspecial2(unsigned char key) 
+{  
+	if(estado==JUEGO) 
+		mundo.teclaEspecial2(key);
+} 
+
+
+void Coordinador::mueve()
+{  
+	if(estado==JUEGO) 
+		mundo.Mover(); 
+} 
+
+
+
+>>>>>>> f1c7d58b8deac8817737c58160bfbe88dda22ed7
--- src/Coordinador.h
+++ src/Coordinador.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #pragma once
 #include "Mundo.h"
 
@@ -21,3 +22,28 @@ protected:
 	Estado estado;
 };
 
+=======
+#pragma once
+#include "Mundo.h"
+
+class Coordinador
+{
+public:
+	Coordinador(void);
+	~Coordinador(void);
+
+	void Tecla(unsigned char key);
+//	void tecla(unsigned char key);  
+	void teclaEspecial(unsigned char key); 
+	void teclaEspecial2(unsigned char key); 
+	void mueve(void); 
+	void dibuja(void);
+
+
+protected:
+	Mundo mundo;
+	enum Estado {INICIO, JUEGO, GAMEOVER, FIN};  
+	Estado estado;
+};
+
+>>>>>>> f1c7d58b8deac8817737c58160bfbe88dda22ed7
